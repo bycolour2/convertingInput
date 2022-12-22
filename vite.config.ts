a7ae69b78ba1438fedc387a764121ff8f0ca4ff8
bycolour2 <ruskir7@gmail.com>
2022-12-22 18:16:27 +0300

    fix: deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // to gh-pages change to "/{repo}/" && use HashRouter instead of BrowserRouter
+  base: '/convertingInput/', // to gh-pages change to "/{repo}/" && use HashRouter instead of BrowserRouter
   plugins: [react(), tsconfigPaths()],
   server: {
     // open: true,
